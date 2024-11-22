67802698888f9cd3d133cdcc98036959429ee533
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-11-22 17:55:51 +0530

    config nvim

--- lua/myNvim/lazy.lua
+++ lua/myNvim/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("myNvim.plugins",{
+require("lazy").setup({{import = "myNvim.plugins"},{import = "myNvim.plugins.lsp" }} ,{
   change_detection = {
     notify = false,
   },
